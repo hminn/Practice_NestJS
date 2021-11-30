03aaeafd7464c9d8b952c9ba88700624f5c9385c
hminn <hmkim0008@gmail.com>
2021-11-30 21:38:31 +0900

    유효성 검사용 파이프 추가 : ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 유효성 검사용 파이프, Express.js의 미들웨어와 같은 역할
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
